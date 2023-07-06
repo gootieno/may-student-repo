2e4a1e40a943a173910a4a42ac8bf626700a0feb
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-07-06 13:13:24 -0700

    fetch demo

--- Week8/Day3/solutions/04-fetch/fetch-requests.js
+++ Week8/Day3/solutions/04-fetch/fetch-requests.js
@@ -6,20 +6,30 @@ Paste your code for fetch requests here once you finish each task.
 /* =============== 1. Print the status code of the response =============== */
 
 // Your code here
+fetch("/")
+  .then((response) => {
+    console.log("response object ", response);
+    console.log("response status ", response.status);
 
+    return response.text();
+  })
+  .then((responseData) => console.log("response data ", responseData));
 
+const landingPage = async () => {
+  const response = await fetch("/");
+  const data = await response.text();
+  console.log("async await data ", data);
+};
+
+landingPage()
 
 /* ====== 2. Print true if the status of the response was successful ====== */
 
 // Your code here
 
-
-
 /* =================== 3. Print the Content-Type Header =================== */
 
 // Your code here
-
-
 
 /* ============== 4. Print the body of the response as text =============== */
 
