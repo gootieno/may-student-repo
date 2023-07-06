0199acfbe0dbc1d8f8ff38862f275cdb8127ba62
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-07-06 10:44:45 -0700

    async await & fetch from server helper code

--- Week8/Day3/solutions/03-fetch-from-server/assets/js/your-code.js
+++ Week8/Day3/solutions/03-fetch-from-server/assets/js/your-code.js
@@ -1,5 +1,19 @@
 export function getAllDogs() {
     // Your code here
+    const url = "/dogs";
+    // const headers = {"Content-Type": "Request body's Content-Type"};
+    // Use the URLSearchParams API to format your body as shown below
+    // const body = new URLSearchParams({
+    //     key: "value"
+    // });
+
+    // const options = {
+    //     method: "GET|POST|PUT|DELETE", 
+    //     headers: headers,
+    //     body: body
+    // };
+
+    return fetch(url);
 }
 
 export function getDogNumberTwo() {
